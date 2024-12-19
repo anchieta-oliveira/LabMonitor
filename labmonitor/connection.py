ef14988cb1b11349bc1c3e3c0416fc4f8b975c51
Anchieta Oliveira <anchieta-13@hotmail.com>
2024-12-19 15:17:40 -0300

    impl. Connection

---
+++ labmonitor/connection.py
@@ -0,0 +1,33 @@
+import paramiko
+
+
+class Connection:
+    def __init__(self, ip:str, username:str, password:str):
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.ssh = self.get_connection()
+
+
+    def set_connection(self, ip:str, username:str, password:str):
+        self.ip = ip
+        self.username = username
+        self.password = password
+
+    def get_connection(self):
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(self.ip, username=self.username, password=self.password, timeout=10)
+            self.ssh = ssh
+        except Exception as e:
+            raise RuntimeError(f"Erro na conexão: {e}")
+        
+        return ssh
+    
+    def execute_ssh_command(self, command):
+        try:
+            stdin, stdout, stderr = self.ssh.exec_command(command)
+            return stdout.read().decode().strip()
+        except Exception as e:
+            raise RuntimeError(f"Erro ao executar comando '{command}': {e}")
