95e0ce6fec151ed4124716a60b6b36c8d00098f2
Anchieta Oliveira <anchieta-13@hotmail.com>
2024-12-20 09:00:59 -0300

    alteração no metodo da memoria e cpu

--- labmonitor/monitor.py
+++ labmonitor/monitor.py
@@ -5,32 +5,42 @@ class Monitor:
         self.connection = connection
     
     def get_usage_cpu(self):
-        cpu_usage = float(self.connection.execute_ssh_command("top -bn1 | grep 'Cpu(s)' | awk '{print $2+$4}'").replace(',', '.'))
+        cpu_usage = float(self.connection.execute_ssh_command("top -bn1 | grep -i 'Cpu(s)' | awk '{print $2+$4}'").replace(',', '.'))
         return {"cpu_info": {"cpu_usage_percentage": cpu_usage}}
 
     def get_usage_gpu(self):
         gpu_info = []
         gpu_output = self.connection.execute_ssh_command("nvidia-smi --query-gpu=index,name,memory.used,memory.total,utilization.gpu --format=csv,noheader,nounits")
         for line in gpu_output.split("\n"):
-            gpu_index, name, mem_used, mem_total, gpu_util = line.split(", ")
-            gpu_info.append({
-                "gpu_index": int(gpu_index),
-                "name": name,
-                "memory_used": float(mem_used) / 1024,
-                "memory_total": float(mem_total) / 1024,
-                "utilization_gpu": gpu_util
-            })
+            try:
+                gpu_index, name, mem_used, mem_total, gpu_util = line.split(", ")
+                gpu_info.append({
+                    "gpu_index": int(gpu_index),
+                    "name": name,
+                    "memory_used": float(mem_used) / 1024,
+                    "memory_total": float(mem_total) / 1024,
+                    "utilization_gpu": gpu_util
+                })
+            except Exception as e:
+                print(f"Erro GPU {line}\n{e}")
+                
+
         return {"gpu_info": gpu_info}
 
     def get_usage_ram(self):
-        ram_command = "top -bn1 -E g| grep 'Mem' | awk '{print $8, $4+$6}'"
+        ram_command = "free -g | awk '/^Mem/ {print $3, $4, $2}'"
         ram_data = self.connection.execute_ssh_command(ram_command).split()
-        ram_free = float(ram_data[0].replace(',', '.')) 
-        ram_used = float(ram_data[1].replace(',', '.')) - ram_free
-        return {"ram_info": {
-                            "ram_used": ram_used,
-                            "ram_free": ram_free,
-                            "total_ram": ram_used + ram_free}}
+        ram_used = float(ram_data[0])
+        ram_free = float(ram_data[1])
+        total_ram = float(ram_data[2])
+
+        return {
+            "ram_info": {
+                "ram_used": ram_used,
+                "ram_free": ram_free,
+                "total_ram": total_ram
+            }
+        }
     
 
     def get_usage_disk(self):
