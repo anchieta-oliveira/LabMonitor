c5fbe05ad5eec1fa6b0ce5f238fba43a31b2f878
Anchieta Oliveira <anchieta-13@hotmail.com>
2024-12-20 10:07:42 -0300

    imple da função de monitoramneto

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import os
 import sys
 import argparse as ap
@@ -9,7 +10,14 @@ parser = ap.ArgumentParser(description='Monitoramento de recurso de maquinas Lin
 parser.add_argument('-f','--file', help='Arquivo de configurações.')
 parser.add_argument('-s', '--start', action='store_true', help='Start Deshbord.')
 parser.add_argument('-sh', '--histoy', action='store_true', help='Salvar histórico das maquinas.')
+parser.add_argument('-ph','--path_history', default=os.path.dirname(os.path.abspath(__file__)), help='Arquivo de configurações.')
 args = parser.parse_args()
+
+if args.histoy:
+    from labmonitor.monitor_history import exec_monitor_history
+    print(args.path_history)
+    p = multiprocessing.Process(target=exec_monitor_history, args=(args.path_history,))
+    p.start()
 
 if args.start:
     try:
