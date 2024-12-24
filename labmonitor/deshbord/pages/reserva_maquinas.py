99bba34db59bed7801f3af269ec74930cb975ccd
Anchieta Oliveira <anchieta-13@hotmail.com>
2024-12-24 19:34:05 -0300

    impl. page reserva de maquinas

---
+++ labmonitor/deshbord/pages/reserva_maquinas.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+import pandas as pd
+import streamlit as st
+from labmonitor.monitor import Monitor
+from labmonitor.connection import Connection
+
+st.markdown("# Agendamento de Máquinas")
+st.sidebar.markdown("# Agendamento de Máquinas")
+
+
