56388fd281dab66e211006ad986cb272385e460f
Anchieta Oliveira <anchieta-13@hotmail.com>
2024-12-24 14:55:51 -0300

    impl. de ações no usuario, criar, remover e add ao grupo sudo.

--- labmonitor/deshbord/pages/manage_users.py
+++ labmonitor/deshbord/pages/manage_users.py
@@ -2,10 +2,80 @@ import os
 import subprocess
 import pandas as pd
 import streamlit as st
-import plotly.express as px
-
-from labmonitor.connection import Connection
 from labmonitor.monitor import Monitor
+from labmonitor.connection import Connection
+
+# Botão para criar novo usuário
+def adduser():
+    with st.form("criar_usuario_form", clear_on_submit=True):
+        username = st.text_input("Nome do Usuário", placeholder="Digite o nome do usuário")
+        password = st.text_input("Senha do Usuário", type="password")
+        is_sudo = st.checkbox("Adicionar ao grupo sudo")
+        useradm = st.text_input("Usuário Administrador (sudo)")
+        passadm = st.text_input("Senha Administrador (sudo)", type="password")
+        
+        criar = st.form_submit_button("Criar Usuário")
+
+        if criar:
+            if not username or not password or not useradm or not passadm:
+                st.error("Por favor, preencha todos os campos.")
+            else:
+                try:
+                    con_adm = Connection(ip=df_filtrado['ip'].iloc[0], username=useradm, password=passadm)
+                    mon_adm = Monitor(con_adm)
+                    mon_adm.add_new_user(username, password, passadm)
+                    if is_sudo:
+                        mon_adm.add_sudo_grup(username, passadm)
+                    st.success(f"Usuário '{username}' criado com sucesso.")
+                    
+                except Exception as e:
+                    st.error(f"Erro ao criar o usuário: {e}")
+        
+# Botão para excluir usuário
+def removeuser():
+
+    with st.form("excluir_usuario_form", clear_on_submit=True):
+        selected_user = st.selectbox("Selecione o usuário para excluir:", users_df["Usuário"])
+        useradm = st.text_input("Usuário Administrador (sudo)")
+        passadm = st.text_input("Senha Administrador (sudo)", type="password")
+        
+        excluir = st.form_submit_button("Excluir Usuário")
+
+        if excluir:
+            if not useradm or not passadm:
+                st.error("Por favor, preencha todos os campos.")
+            else:
+                try:
+                    con_adm = Connection(ip=df_filtrado['ip'].iloc[0], username=useradm, password=passadm)
+                    mon_adm = Monitor(con_adm)
+                    mon_adm.remove_user(selected_user, passadm)
+                    st.success(f"Usuário '{selected_user}' excluído com sucesso.")
+                    
+                except Exception as e:
+                    print(e)
+                    st.error(f"Erro ao criar o usuário: {e}")
+
+# Botão Tornar Sudo
+def addsudo():
+    with st.form("addsudo_usuario_form", clear_on_submit=True):
+        selected_user = st.selectbox("Selecione o usuário para tornar sudo:", users_df["Usuário"])
+        useradm = st.text_input("Usuário Administrador (sudo)")
+        passadm = st.text_input("Senha Administrador (sudo)", type="password")
+        
+        tornar_sudo = st.form_submit_button("Adicionar ao sudo")
+
+        if tornar_sudo:
+            if not useradm or not passadm:
+                st.error("Por favor, preencha todos os campos.")
+            else:
+                try:
+                    con_adm = Connection(ip=df_filtrado['ip'].iloc[0], username=useradm, password=passadm)
+                    mon_adm = Monitor(con_adm)
+                    mon_adm.add_sudo_grup(selected_user, passadm)
+                    st.success(f"Usuário '{selected_user}' sudo com sucesso.")
+                    
+                except Exception as e:
+                    st.error(f"Erro ao criar o usuário: {e}")
 
 st.markdown("# Gerenciar usuários")
 st.sidebar.markdown("# Gerenciar usuários.")
@@ -17,45 +87,33 @@ df_filtrado = df[df['name'] == maquina_selecionada].dropna(axis=1, how='all')
 c = Connection(df_filtrado['ip'].iloc[0], df_filtrado['username'].iloc[0], df_filtrado['password'].iloc[0])
 m = Monitor(c)
 
-users_df = pd.DataFrame([
-    {"Usuário": usuario, "Grupos": grupos}
-    for usuario, grupos in m.get_users().items()
-])
+def run_get_user():
+    global users_df 
+    users_df = pd.DataFrame([
+        {"Usuário": usuario, "Grupos": grupos}
+        for usuario, grupos in m.get_users().items()
+    ])
+    
 
-st.subheader("Usuários")
-st.dataframe(users_df, use_container_width=True, hide_index=True)
+    st.subheader("Usuários")
+    st.dataframe(users_df, use_container_width=True, hide_index=True)
 
 
-# Botão para criar novo usuário
-if st.button("Criar Novo Usuário"):
-    with st.form("criar_usuario_form", clear_on_submit=True):
-        nome_usuario = st.text_input("Nome do Usuário", placeholder="Digite o nome do usuário")
-        senha_usuario = st.text_input("Senha do Usuário", type="password")
-        is_sudo = st.checkbox("Adicionar ao grupo sudo")
-        senha_sudo = st.text_input("Senha de Administrador (sudo)", type="password")
-        
-        # Botão de submissão
-        criar = st.form_submit_button("Criar Usuário")
-        
-        if criar:
-            if not nome_usuario or not senha_usuario or not senha_sudo:
-                st.error("Por favor, preencha todos os campos.")
-            else:
-                # Comando para criar o usuário
-                try:
-                    # Adiciona o usuário
-                    comando_add_user = f"echo {senha_sudo} | sudo -S useradd -m {nome_usuario}"
-                    subprocess.run(comando_add_user, shell=True, check=True, text=True)
-                    
-                    # Configura a senha do usuário
-                    comando_set_senha = f"echo '{nome_usuario}:{senha_usuario}' | sudo -S chpasswd"
-                    subprocess.run(comando_set_senha, shell=True, check=True, text=True)
-                    
-                    # Adiciona ao grupo sudo se necessário
-                    if is_sudo:
-                        comando_add_sudo = f"echo {senha_sudo} | sudo -S usermod -aG sudo {nome_usuario}"
-                        subprocess.run(comando_add_sudo, shell=True, check=True, text=True)
-                    
-                    st.success(f"Usuário '{nome_usuario}' criado com sucesso.")
-                except subprocess.CalledProcessError as e:
-                    st.error(f"Erro ao criar o usuário: {e}")
+run_get_user()
+
+try: 
+    st.subheader("Usuários logados")
+    st.dataframe(m.logged_users()['logged_users'], use_container_width=True, hide_index=True)
+except Exception as e:
+    st.error(f"Erro ao carregar usuários logados: {e}")
+
+action = st.selectbox("Escolha uma ação", ["Selecione", "Criar Novo Usuário", "Adicionar ao sudo", "Excluir Usuário"])
+fun = {"Selecione": print, "Adicionar ao sudo": addsudo, "Criar Novo Usuário": adduser, "Excluir Usuário": removeuser}
+
+if "action_state" not in st.session_state: 
+    st.session_state.action_state = action
+
+try:
+    fun[action]()
+except Exception as e:
+    st.error(f"Erro ao selecionar ação: {e}")
