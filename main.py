403c0e8e4dd9bda1adda36d652684aad43d54822
Anchieta Oliveira <anchieta-13@hotmail.com>
2024-12-26 10:04:57 -0300

    impl. de moitorar a fila

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import multiprocessing
 import os
 import sys
 import argparse as ap
+import multiprocessing
+
+
 
 
 parser = ap.ArgumentParser(description='Monitoramento de recurso de maquinas Linux em rede local e apresentação em Deshbord.',
@@ -10,14 +12,23 @@ parser = ap.ArgumentParser(description='Monitoramento de recurso de maquinas Lin
 parser.add_argument('-f','--file', help='Arquivo de configurações.')
 parser.add_argument('-s', '--start', action='store_true', help='Start Deshbord.')
 parser.add_argument('-sh', '--histoy', action='store_true', help='Salvar histórico das maquinas.')
+parser.add_argument('-sq', '--queue', action='store_true', help='Monitorar fila. ')
 parser.add_argument('-ph','--path_history', default=os.path.dirname(os.path.abspath(__file__)), help='Arquivo de configurações.')
 args = parser.parse_args()
 
 if args.histoy:
     from labmonitor.monitor_history import exec_monitor_history
-    print(args.path_history)
     p = multiprocessing.Process(target=exec_monitor_history, args=(args.path_history,))
     p.start()
+
+if args.queue:
+    from labmonitor.queue import Queue
+    from labmonitor.data import Data
+    data = Data()
+    q = Queue(data=data)
+    pq = multiprocessing.Process(target=q.monitor)
+    pq.start()
+
 
 if args.start:
     try:
