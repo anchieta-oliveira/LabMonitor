68ba6f9ce85f1e349ca3e9956b833f7e77986dd5
Anchieta Oliveira <anchieta-13@hotmail.com>
2024-12-26 22:18:37 -0300

    impl. gpu_use

--- labmonitor/deshbord/pages/statistics.py
+++ labmonitor/deshbord/pages/statistics.py
@@ -25,5 +25,20 @@ def cpu_use():
     st.plotly_chart(fig_cpu_use)
 
 
+def gpu_use():
+    st.subheader("Uso de GPU (H)")
+    maquinas = df['Name'].unique()
+    df_maquinas_gpu = [{m: (df[df['Name'] == m].loc[:, df.columns.str.contains(r"gpu.*utilization|gpu_.*_name", case=False, regex=True)].dropna(axis=1, how='all'))} for m in maquinas]
+    r = []
+    for df_gpu in df_maquinas_gpu:
+        maq = [*df_gpu][0]
+        df_maq = df_gpu[maq]
+        for i, (col_uti, col_name) in enumerate(zip(df_maq.loc[:,df_maq.columns.str.contains(r"gpu.*utilization", case=False, regex=True)], df_maq.loc[:,df_maq.columns.str.contains(r"gpu_.*_name", case=False, regex=True)])):
+            r.append({"Máquina": maq, "GPU Usage (H)": (df_maq[col_uti]/100).sum(), "GPU Name": df_maq[col_name].iloc[-1]+ str(i*" ")})
+    df_gpu_usage = pd.DataFrame(r)
+    fig_gpu_use = px.sunburst(df_gpu_usage, names="Máquina", path=['Máquina', 'GPU Name'], values='GPU Usage (H)')
+    st.plotly_chart(fig_gpu_use)
+
 cpu_use()
+gpu_use()
 
