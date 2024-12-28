b895fb3ff853b9d2519f9c53ce2ec3a5f0413a52
Anchieta Oliveira <anchieta-13@hotmail.com>
2024-12-28 09:40:01 -0300

    impl de update_gpu para pegar os recursos insladdos na maquina

--- labmonitor/queue_job.py
+++ labmonitor/queue_job.py
@@ -2,8 +2,11 @@
 
 import os
 import pandas as pd
-from labmonitor.connection import Connection
 from labmonitor.data import Data
+from labmonitor.monitor import Monitor
+from labmonitor.connection import Connection
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 
 class QueueJob:
@@ -31,6 +34,53 @@ class QueueJob:
         self.df.to_excel("queue_job.xlsx", index=False)
         return self.df
     
+    def update_gpu(self):
+        def run(ip, name, user, pw):
+            try:
+                print(f"Conectando a {ip}...")
+                results = {}
+                c = Connection(ip, user, pw)
+                m = Monitor(c)
+            except Exception as e:
+                print(f"Erro ao conectar a {ip}: {e}")
+                return name, None
+            try:
+                results.update(m.get_usage_gpu())
+            except Exception as e:
+                results.update({"gpu_info": []})
+                print(f"Erro ao obter informações de GPU de {ip}: {e}")
+            return name, results
+        
+        ips = self.data.machines['ip'].to_list()
+        names = self.data.machines['name'].to_list()
+        usernames = self.data.machines['username'].to_list()
+        passwords = self.data.machines['password'].to_list()
+
+        results = {}
+
+        with ThreadPoolExecutor(max_workers=len(ips)) as executor:
+            futures = [
+                executor.submit(run, ip, name, user, pw)
+                for ip, name, user, pw in zip(ips, names, usernames, passwords)
+            ]
+            for future in as_completed(futures):
+                name, stats = future.result()
+                if stats:
+                    results[name] = stats
+
+        data_gpu = []
+        for name, stats in results.items():
+            row = {"name": name}
+            for gpu in stats["gpu_info"]:
+                gpu_index = gpu["gpu_index"]
+                row[f"GPU_{gpu_index}_Name"] = gpu["name"]
+                row[f"GPU_{gpu_index}_status"] = ""
+
+            data_gpu.append(row)
+
+        self.data.machines = pd.merge(self.machines, pd.DataFrame(data_gpu), on="name")
+        self.data.save_machines()
+
     def copy_dir(self, ip_origin, username_origin, password_origin, ip_exc, username_exc, password_exc, path_origin:str, path_exc:str):
         cmd = f"""sshpass -p '{password_origin}' ssh {username_origin}@{ip_origin} "echo '{password_origin}' | sudo -S sshpass -p '{password_exc}' scp -r {path_origin} {username_exc}@{ip_exc}:{path_exc}" """
         os.system(cmd)
