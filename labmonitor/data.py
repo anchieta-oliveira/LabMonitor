e75297e0f6fcdb6ba11e0c4d835e4a62b5ad1c4c
Anchieta Oliveira <anchieta-13@hotmail.com>
2024-12-28 10:02:47 -0300

    impl de save_machines para atualziar infos das maquinas.

--- labmonitor/data.py
+++ labmonitor/data.py
@@ -7,11 +7,17 @@ current_dirname_path = os.path.dirname(os.path.abspath(__file__))
 class Data:
     def __init__(self):
         self.machines = pd.DataFrame()
+        self.path_machines = ""
         self.email = {}
 
     def read_machines(self, path:str=f"{current_dirname_path}/../machines.xlsx"):
+        self.path_machines = path
         self.machines = pd.read_excel(path)
         return self.machines
+
+    def save_machines(self, path:str=f""):
+        if path == "": path = self.path_machines
+        self.machines.to_excel(path, index=False)
 
     def read_email(self, path:str=f"{current_dirname_path}/../email.json"):
         try:
