2152e71bc6a704ed95ad894b9becb5b61ec9348c
Anchieta Oliveira <anchieta-13@hotmail.com>
2025-01-01 13:09:52 -0300

    impl p/ mostar  gpu_requested

--- labmonitor/deshbord/pages/fila_de_trabalho.py
+++ labmonitor/deshbord/pages/fila_de_trabalho.py
@@ -74,7 +74,7 @@ data = Data(); data.read_machines(path=f"{sys.argv[1]}/machines_job.xlsx")
 queue = QueueJob(data=data)
 
 st.subheader("Fila de trabalhos")
-st.dataframe(queue.df[queue.df['status'] != 'finalizado'][['name', 'username', 'job_name','status', 'submit', 'n_cpu', 'gpu_name']], use_container_width=True, hide_index=True)
+st.dataframe(queue.df[queue.df['status'] != 'finalizado'][['name', 'username', 'job_name','status', 'submit', 'n_cpu', 'gpu_requested', 'gpu_name']], use_container_width=True, hide_index=True)
 
 
 def nenhum():
