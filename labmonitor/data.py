fec24e9c29801881f618e934e6f3cfbaaafd8638
Anchieta Oliveira <anchieta-13@hotmail.com>
2025-01-13 11:10:02 -0300

    impl. read_users e users

--- labmonitor/data.py
+++ labmonitor/data.py
@@ -8,6 +8,8 @@ class Data:
     def __init__(self):
         self.machines = pd.DataFrame()
         self.path_machines = ""
+        self.path_users = ""
+        self.users = pd.DataFrame()
         self.email = {}
 
     def read_machines(self, path:str=f"{current_dirname_path}/../machines.xlsx"):
@@ -25,6 +27,17 @@ class Data:
             
         except Exception as e:
             print("Erro ao salvar o arquivo:", e)
+
+
+    def read_users(self, path:str=f"{current_dirname_path}/../users.xlsx") -> pd.DataFrame:
+        try:
+            self.path_users = path
+            self.users = pd.read_excel(path)
+        
+        except Exception as e:
+            print(f"Não foi possivel carregar as informações de usuários: {e}")
+
+        return self.users
 
 
     def read_email(self, path:str=f"{current_dirname_path}/../email.json"):
