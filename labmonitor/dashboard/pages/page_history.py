2de6e5fbc7c293dbf2ede8a17c808b4306b32bf6
Anchieta Oliveira <anchieta-13@hotmail.com>
2025-01-17 14:54:20 -0300

    tradução

--- labmonitor/dashboard/pages/page_history.py
+++ labmonitor/dashboard/pages/page_history.py
@@ -10,28 +10,28 @@ import plotly.express as px
 # Main
 ############################################################################################################
 
-st.markdown("# Histórico")
-st.sidebar.markdown("# Histórico de uso.")
+st.markdown("# History")
+st.sidebar.markdown("# History of use.")
 
 df = pd.read_csv(f"{os.path.dirname(os.path.abspath(__file__))}/../../../history.csv")
 
 
 maquinas = df['Name'].unique()
-maquina_selecionada = st.selectbox('Escolha a máquina', maquinas)
+maquina_selecionada = st.selectbox('Choose a machine', maquinas)
 
 df_filtrado = df[df['Name'] == maquina_selecionada].dropna(axis=1, how='all')
 
 csv = df_filtrado.to_csv().encode("utf-8")
 st.download_button(
-    label=f"Download dados da {maquina_selecionada} em CSV",
+    label=f"Download {maquina_selecionada} in CSV",
     data=csv,
     file_name=f"data_{maquina_selecionada}.csv",
     mime="text/csv", use_container_width=True
 )
 # --- Gráfico de uso de CPU ---
 fig_cpu = px.line(df_filtrado, x='Timestamp', y='CPU Usage (%)', 
-                  title=f'Uso de CPU para {maquina_selecionada}', 
-                  labels={'Timestamp': 'Tempo', 'CPU Usage (%)': 'Uso de CPU (%)'},
+                  title=f'CPU usage for {maquina_selecionada}', 
+                  labels={'Timestamp': 'Time', 'CPU Usage (%)': 'CPU usage (%)'},
                   line_shape='linear', markers=True)
 fig_cpu.update_yaxes(range=[0, 100]) 
 
@@ -39,8 +39,8 @@ st.plotly_chart(fig_cpu)
 
 # --- Gráfico de uso de RAM ---
 fig_ram = px.line(df_filtrado, x='Timestamp', y='RAM Used (GB)', 
-                  title=f'Uso de RAM para {maquina_selecionada}', 
-                  labels={'Timestamp': 'Tempo', 'RAM Used (GB)': 'Uso de RAM (GB)'},
+                  title=f'Use of RAM for {maquina_selecionada}', 
+                  labels={'Timestamp': 'Time', 'RAM Used (GB)': 'RAM Used (GB)'},
                   line_shape='linear', markers=True)
 fig_ram.update_yaxes(range=[0, df_filtrado['Total RAM (GB)'].iloc[-1]]) 
 st.plotly_chart(fig_ram)
@@ -54,15 +54,15 @@ gpu_labels = {
 
 if gpus:
     fig_gpu = px.line(df_filtrado, x='Timestamp', y=gpus, 
-                      title=f'Uso de GPUs para {maquina_selecionada}', 
-                      labels={'Timestamp': 'Tempo', 'value': 'Uso (%)'},
+                      title=f'Use of GPUs for {maquina_selecionada}', 
+                      labels={'Timestamp': 'Time', 'value': 'Usage (%)'},
                       line_shape='linear', markers=True)
     fig_gpu.update_yaxes(range=[0, 100])
 
     fig_gpu.for_each_trace(lambda trace: trace.update(name=gpu_labels[trace.name]))
     st.plotly_chart(fig_gpu)
 else:
-    st.write("Nenhuma coluna de GPU encontrada no arquivo.")
+    st.write("No GPU column found in the file.")
 
 
 
@@ -76,12 +76,12 @@ gpu_labels_vram = {
 
 if gpus:
     fig_gpu_vram = px.line(df_filtrado, x='Timestamp', y=gpus, 
-                      title=f'Uso de VRAM para {maquina_selecionada}', 
-                      labels={'Timestamp': 'Tempo', 'value': 'Uso de VRAM (GB)'},
+                      title=f'Use of VRAM for {maquina_selecionada}', 
+                      labels={'Timestamp': 'Time', 'value': 'VRAM usage (GB)'},
                       line_shape='linear', markers=True)
     #fig_gpu.update_yaxes(range=[0, 100])  # Limita o eixo Y de 0 a 100%
 
     fig_gpu_vram.for_each_trace(lambda trace: trace.update(name=gpu_labels_vram[trace.name]))
     st.plotly_chart(fig_gpu_vram)
 else:
-    st.write("Nenhuma coluna de GPU encontrada no arquivo.")
+    st.write("No GPU column found in the file.")
